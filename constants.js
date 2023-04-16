68256262b521f29963b5215d7dfbd8225ef1a0ab
VittorioSc <128059723+VittorioSc@users.noreply.github.com>
2023-04-17 00:43:13 +0200

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,38 @@
+﻿const RESULTS_MAP = {
+'blep': {
+title: 'La forza della natura',
+contents: "Improvviso, impavido e pieno di energia, la chitarra è il vostro strumento di potere. Avete scelto come vostro Guitar Hero il mitico Angus Young, che già di per sè la dice molto lunga su di voi! Non riuscite a stare fermi un secondo, avete sempre qualcosa per le mani (probabilmente la vostra chitarra) e per la testa (magari un nuovo solo), ma una cosa è sicura: quando salite sul palco siete più energici di una Super Nova.",
+},
+'burger': {
+title: 'Il sognatore',
+contents: "Con la vostra chitarra amate costruire dei veri e propri paesaggi sonori, creando un sound che sognate faccia scuola! David Gilmour è il Guitar Hero che fa proprio al caso vostro: apparentemente freddo e distaccato sul palco ma che in realtà nasconde in quei piccoli movimenti tutto il genio dei suoi riff. Siete anticonformisti, non vi importa se nella vostra interpretazione inserirete 5 minuti di sola strumentale nè tanto più se a qualcuno non sarà gradito. Che altro aggiungere se non un, ' I'll see you on the dark side of the moon '?",
+},
+'cart': {
+title: 'Il re del barocco',
+contents: "Interpreti unici, combinate l'estetica classica con la forza del rock, creando un sound distintivo tutto vostro. Yngwie Malmsteen è sicuramente il vostro spirito guida perfetto! Implacabile, tecnico fino all'estremo e incredibile nella resa emotiva di ciò che suona, un po' come voi che lo avete scelto del resto. Piccolo suggerimento: quando deciderete di scavare dei solchi sul manico della vostra Stratocaster per farci scorrere le dita ancora più velocemente proteggete bene gli occhi!",
+},
+'dopey': {
+title: 'Lo stacanovista',
+contents: "Quanto lavoro ci vuole per creare un riff o un solo che si insinua nella testa? Forse spenderete tutta la vita per rispondere a questa domanda. Slash è sicuramente il chitarrista più folle tra quelli presentati, quindi il parallelismo con i vostri modi estrosi è praticamente perfetto! Sognate di avere (o avete se siete più fortunati) l'iconica Gibson Les Paul in combo con un Marshall valvolare che renda al meglio il vostro suono: potente e deciso, ma mai scontato. State solo attenti a non farvi aggredire da uno spettatore quando sarete sul palco come accadde al vostro Guitar Hero qualche anno fa!",
+},
+'happy': {
+title: 'Il mago del progressive',
+contents: "Creatori di riff intricati, unite l'abilità tecnica alla melodia in modo maestoso. Se vi è mai capitato di far ascoltare a qualcuno uno dei vostri emozionanti assoli e l'avete visto piangere e chissà magari tatuarsi il ritornello del pezzo più importante per lui (n.d.r.: come nel mio caso) il mitico John Petrucci è il Guitar Hero che fa per voi! Siete espressivi, graffianti e soprattutto suonate talmente bene e velocemente che sembra abbiate 8 dita per mano. Ad maiora e soprattutto ricordate che ' The Spirit Carries On '.",
+},
+'nerd': {
+title: 'Il virtuoso',
+contents: "Tecnica impeccabile, una combinazione di tecnologia e talento che porta la chitarra a livelli mai immaginati prima. Si potrebbero dire molte cose su Steve Vai, il vostro spirito guida, come il fatto che sia uno dei pochissimi chitarristi al mondo a riuscire a suonare una chitarra con 3 manici da solo senza il minimo sforzo, ma questo di sicuro lo sapete. E se ne siete capaci anche voi beh, sappiate che il 99% dei vostri amici chitaristi vi starà sicuramente invidiando! Tecnici, mai ripetitivi e con la capacità di rinnovare sempre le vostre composizioni, i tasti in palissandro della vostra Ibanez Jem ne sono i testimoni."
+},
+'shy': {
+title: 'Il maestro delle corde',
+contents: "Con la vostra abilità e grande cultura musicale sapete combinare la tradizione del pop italiano con l'energia del rock. Distanti dal mondo chiassoso dei chitarristi internazionali, preferite al contrario utilizzare le vostre grandi abilità con la pentatonica e la scala minore armoncia in terra nostrana, un po' come il vostro Guitar Hero Dodi Battaglia, iconico musicista dei Pooh. E con tutta l'energia che vi ritrovate è proprio il caso di chiederselo: ' Chi fermerà la musica? '.",
+},
+'sleeping': {
+title: 'Il visionario',
+contents: "Geni creativi, avete probabilmente una chitarra dallo stile unico. E probabilmente anche un bel coraggio per decidere che Brian May, nominato da Total Guitar il più grande chitarrista di tutti i tempi, è il vostro spirito guida! Ma del resto non stupisce se vi si conosce abbastanza: siete intrepidi e anticonformisti, talmente alternativi che come il vostro mito avrete passato anni a pensare come costruire una chitarra che suonasse non come le altre, piuttosto come se fosse un'estensione di voi stessi. Piccolo suggerimento: non fate come May che ha rubato i bottoni in madreperla della madre per creare i segnatasti della vostra chitarra homemade, potrebbe arrivarvi una ciabatta dalla sicuramente meno clemenete vostra di madre!",
+},
+'sleepy': {
+title: 'La leggenda vivente',
+contents: "Sognate di influenzare generazioni di artisti, costruendo un sound che segnerà indelebilmente la storia della musica. Se il vostro Guitar Hero è il protagonista del celeberrimo graffito a lui dedicato ('Clapton is God') c'è veramente poco da dire su di voi: adorate il blues e il jazz e non perdere mai occasione per dimostrare a tutti che sì, siete molto talentuosi sulla chitarra elettrica, ma che quando imbracciate la vostra chitarra acustica potreste letteralmente avere il mondo ai vostri piedi!",
+}
+};
